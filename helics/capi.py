0c100064d8c3d431914ada0dbb6f8e7f503e6736
Dheepak Krishnamurthy <me@kdheepak.com>
2020-10-31 23:53:44 -0600

    Remove strip-hints

--- helics/capi.py
+++ helics/capi.py
@@ -10,10 +10,6 @@ try:
 except ImportError:
     pass
 
-from strip_hints import strip_on_import
-
-strip_on_import(__file__, to_empty=False, no_ast=False, no_colon_move=False, only_assigns_and_defs=False, py3_also=False)
-
 from . import _build
 
 lib = _build.lib
