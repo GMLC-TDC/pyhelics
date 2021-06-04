dc4f163e694a9831f6cd496e3dc48219bd9de824
Dheepak Krishnamurthy <me@kdheepak.com>
2021-06-04 12:06:07 -0600

    Remove HELICS_ZMQ_SUBPROJECT

--- setup.py
+++ setup.py
@@ -199,7 +199,6 @@ class HELICSCMakeBuild(build_ext):
 
         cmake_args = [
             "-DHELICS_DISABLE_GIT_OPERATIONS=OFF",
-            "-DHELICS_ZMQ_SUBPROJECT=ON",
             "-DCMAKE_BUILD_TYPE=Release",
             "-DCMAKE_INSTALL_PREFIX={}".format(extdir),
         ]
