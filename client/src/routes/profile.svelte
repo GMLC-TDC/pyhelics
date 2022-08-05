8c9bd5a6272b83a08ec2d885887aacaec79da7dc
Dheepak Krishnamurthy <me@kdheepak.com>
2022-08-05 09:08:14 -0600

    chore: Update files ♻️

--- client/src/routes/profile.svelte
+++ client/src/routes/profile.svelte
@@ -49,14 +49,16 @@
           class="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
           on:click={handleClearClick}>Clear</button
         >
+        <div class="flex flex-col justify-center">
+          <Switch
+            bind:value={$data.profile_toggle_value}
+            label=""
+            design="multi"
+            options={["real_time", "simulation_time"]}
+            fontSize={12}
+          />
+        </div>
       </div>
-      <Switch
-        bind:value={$data.profile_toggle_value}
-        label="Choose a theme"
-        design="multi"
-        options={["real_time", "simulation_time"]}
-        fontSize={12}
-      />
     </div>
   </div>
 
